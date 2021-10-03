6732d6e4a0859fd4d9279a8773ab7f0df12a2e88
Frisonjr <jair.frison@gmail.com>
2021-10-02 22:48:25 -0300

    expense item files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h1>LET'S FUCKING GOOOOOOOOOOOOOOOOOOOOOOO</h1>
       <p>Testing my html elements</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
