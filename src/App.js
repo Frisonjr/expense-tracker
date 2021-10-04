46cace920a23f7062c4990fdeba6a3472ac2bfd0
FrisonJr <jair.frison@gmail.com>
2021-10-04 11:13:47 -0300

    expense items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 294.87,
+      date: new Date(2021, 10, 4),
+    },
+    {
+      title: "Bandeclay",
+      amount: 10,
+      date: new Date(2021, 10, 1),
+    },
+  ];
   return (
     <div>
-      <h1>LET'S FUCKING GOOOOOOOOOOOOOOOOOOOOOOO</h1>
+      <h1>LET'S GOOOOOOOOOOOOOOOOOOOOOOO</h1>
       <p>Testing my html elements</p>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
     </div>
   );
 }
