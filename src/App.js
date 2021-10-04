c96b851ef92db1daece3ee3903b475db191a3c72
FrisonJr <jair.frison@gmail.com>
2021-10-04 17:37:11 -0300

    expenses and card

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
+
+import Expenses from './components/Expenses';
+
 
 function App() {
   const expenses = [
     {
       title: "Car Insurance",
       amount: 294.87,
-      date: new Date(2021, 10, 4),
+      date: new Date(2452, 10, 1),
     },
     {
       title: "Bandeclay",
       amount: 10,
-      date: new Date(2021, 10, 1),
+      date: new Date(1989, 11, 2),
+    },
+    {
+      title: "sdasd",
+      amount: 10,
+      date: new Date(2021, 12, 25),
+    },
+    {
+      title: "gdgdfg",
+      amount: 10,
+      date: new Date(1998, 9, 1),
     },
   ];
+
   return (
     <div>
-      <h1>LET'S GOOOOOOOOOOOOOOOOOOOOOOO</h1>
-      <p>Testing my html elements</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
+      <div>LETS F#CKING GOOOOOOOOOOO</div>
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
-import './ExpenseItem.css'
+import React from 'react';
 
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
