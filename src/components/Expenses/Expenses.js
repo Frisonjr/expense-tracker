9ab175b8b5897f491913bea6fadd97cfbac1d7ac
Jair Frison Júnior <jair.frison@gmail.com>
2021-10-06 15:19:52 -0300

    clean filter button

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,35 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from "react";
 
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import ExpensesList from './ExpensesList';
-import ExpensesChart from './ExpensesChart';
-import './Expenses.css';
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState(undefined);
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
-  });
+  // const filteredExpenses = props.items.filter((expense) => {
+  //   return expense.date.getFullYear().toString() === filteredYear;
+  // });
+
+  const filteredExpenses = useMemo(() => {
+    if (filteredYear) {
+      return props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+      });
+    } else {
+      return props.items;
+    }
+  }, [filteredYear, props]);
 
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
